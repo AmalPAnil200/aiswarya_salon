a50cd2d0db5d0e7b2f76a8d8bbcda8c96feb73e8
AmalPAnil200 <amalpanil200@gmail.com>
2025-02-10 01:17:51 +0530

    form changed and style changed

--- salon_aiswarya/settings.py
+++ salon_aiswarya/settings.py
@@ -29,11 +29,11 @@ DEBUG = True
 ALLOWED_HOSTS = ['aiswarya-salon.onrender.com']
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  # Change if using another email provider
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = '111amalpanil2002@gmail.com'  # Your email
-EMAIL_HOST_PASSWORD = 'ntre cing zpfy eaar' 
+EMAIL_HOST_USER = 'aiswaryasalon@gmail.com'
+EMAIL_HOST_PASSWORD = 'eipk kmts ward moko' 
 
 # Application definition
 
